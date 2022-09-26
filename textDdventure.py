fa3cd04aa7a6b1282c72f3954383a4ea5fd34c6c
EvanRothenbacher10 <825585@mystma.org>
2022-09-26 11:12:36 -0500

    34

--- textDdventure.py
+++ textDdventure.py
@@ -2,6 +2,8 @@ import os
 from os import *
 import time
 from time import *
+import threading
+from threading import *
 
 # Variables
 
@@ -12,25 +14,32 @@ def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def loadingScrn():
-        print("         Loading \n         .")
+        clear()
+        print("\n         \033[0;32mLoading \n        [.]")
         sleep(.5)
         clear()
-        print("         Loading \n         ..")
+        print("\n         Loading \n        [..]")
         sleep(.5)
         clear()
-        print("         Loading \n         ...")
+        print("\n         Loading \n        [...]")
         sleep(.5)
         clear()
-        print("         Loading \n         ....")
+        print("\n         Loading \n        [....]")
         sleep(.5)
         clear()
-        print("         Loading \n         .....")
+        print("\n         Loading \n        [.....]")
         sleep(.5)
         clear()
-        print("         Loading \n         ......")
+        print("\n         Loading \n        [......]")
         sleep(.5)
         clear()
-        print("         Loading \n         .......")
+        print("\n         Loading \n        [.......]")
+        sleep(.5)
+        clear()
+        print("\n     Loading Complete \n        [.......]\033[0;37m")
+        sleep(0.5)
+        clear()
+        return " "
 
 def startGame(x):
     if x == "Y":
@@ -38,14 +47,22 @@ def startGame(x):
     else:
         return "Error"
 
+def firstQuestion(x):
+    if x == "Y":
+        return "preinted2"
+
 # Calls
 
 clear()
 
-print("WARNING: Game is case sensitive, all answers should be in Y/N format.")
+print("\033[0;31m\n \n        WARNING: Game is case sensitive, all answers should be in Y/N format.")
 
 sleep(5)
 
 clear()
 
-print(startGame(input("Do you want to start the game? \n")))
+print(startGame(input("\033[0;37mDo you want to start the game? \n")))
+
+print("You wake up feeling hungry...", end=".")
+
+print(firstQuestion(input(" Make toast?")))
